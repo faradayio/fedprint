19ef25bae92d44d013f6f4241ccf9e75bd311f78
Andy Rossmeissl <andy@rossmeissl.net>
2010-11-09 14:21:08 -0500

    Separate this out so that Seamus can cache it

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -2,6 +2,12 @@ require 'open-uri'
 
 class ContractsController < ApplicationController
   def index
-    @contracts = Contract.create_from_feed(SimpleRSS.parse(open('https://www.fpds.gov/dbsight/FEEDS/ATOM?FEEDNAME=DETAIL&q=CONTRACT_TYPE:%22AWARD%22&sortBy=LAST_MOD_DATE&desc=Y')))
+    @contracts = Contract.create_from_feed(fpds_feed)
+  end
+  
+  private
+  
+  def fpds_feed
+    SimpleRSS.parse(open('https://www.fpds.gov/dbsight/FEEDS/ATOM?FEEDNAME=DETAIL&q=CONTRACT_TYPE:%22AWARD%22&sortBy=LAST_MOD_DATE&desc=Y'))
   end
 end
