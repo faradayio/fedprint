5239ea78cb43f30093193d963d091840e5aaae87
Seamus Abshere <seamus@abshere.net>
2010-11-09 14:05:34 -0600

    try caching

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -8,6 +8,13 @@ class ContractsController < ApplicationController
   private
   
   def fpds_feed
-    SimpleRSS.parse(open('https://www.fpds.gov/dbsight/FEEDS/ATOM?FEEDNAME=DETAIL&q=CONTRACT_TYPE:%22AWARD%22&sortBy=LAST_MOD_DATE&desc=Y'))
+    cache_key = "#{__FILE__}/fpds_feed"
+    if Rails.cache.exist? cache_key
+      Rails.cache.read cache_key
+    else
+      x = SimpleRSS.parse(open('https://www.fpds.gov/dbsight/FEEDS/ATOM?FEEDNAME=DETAIL&q=CONTRACT_TYPE:%22AWARD%22&sortBy=LAST_MOD_DATE&desc=Y'))
+      Rails.cache.write cache_key, x, :expires_in => 1.hour
+      x
+    end
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,6 @@ Fedprint::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+  
+  config.cache_store = :dalli_store, 'localhost:11211'
 end
-
