c0542d76b0a7f37d62070dfe757043cb2575f2f9
Andy Rossmeissl <andy@rossmeissl.net>
2010-11-11 14:04:58 -0500

    Show recency

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,5 +1,6 @@
 class ContractsController < ApplicationController
   def index
     @contracts = Contract.order('updated_at DESC').limit(10)
+    @last_updated = @contracts.first.updated_at
   end
 end
